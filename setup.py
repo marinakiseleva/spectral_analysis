6add9fa73398e2e9a224a22c5a89e4a9379d992b
Marina Kiseleva <marinaki@vt.edu>
2019-12-04 12:51:44 -0700

    Add setup and init to located module

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+setup(
+    name="specpgm",
+    version="0.1",
+    packages=find_packages(),
+    author="Marina Kisley",
+    author_email="marinaki@email.arizona.edu",
+    description="Probabilistic graphical model for spectral analysis."
+)
