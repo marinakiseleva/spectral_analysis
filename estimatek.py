b729706fc745d38f075805e3b714e3e41c7685b6
Marina Kiseleva <marinaki@vt.edu>
2019-12-04 14:18:14 -0700

    Udpated estimate k to estimate per wavelength

--- estimatek.py
+++ estimatek.py
@@ -27,62 +27,98 @@ pd.set_option('display.max_rows', None)
 ######################################################
 
 
-def get_reflectance_rmse(sid, sample_spectra, k, D):
-    sample_row = sample_spectra[sample_spectra['SpectrumID'] == sid]
-    # source_angle : incidence angle in degrees
-    source_angle = sample_row["SourceAngle"].values[0]
-    mu_0 = get_cosine(source_angle)
-
-    # detect_angle emission/emergence angle in degrees
-    detect_angle = sample_row["DetectAngle"].values[0]
-    mu = get_cosine(detect_angle)
-
-    wavelengths = get_wavelengths(sid, sample_spectra)
-
-    n = sids_n[sid]
+def get_reflectance_error(wavelength, r, n, k, D):
+    """
+    Gets difference between estimated reflectance using Hapke model and reflectance in data, for a wavelength at a certain index
+    :param wavelength: wavelength 
+    :param r: reflectance at wavelength index, scalar
+    :param n: optical constant n, scalar (constant across all wavelengths)
+    :param k: optical constnat k, scalar (for this wavelength)
+    :param D: grain size, scalar
+    """
 
     # Calculated estimated reflectance
-    r_e = get_reflectance_hapke_estimate(mu, mu_0, n, k, D, wavelengths)
-
-    # Get actual reflectance from data
-    r = get_reflectance_spectra(sid, sample_spectra)
+    r_e = get_reflectance_hapke_estimate(mu, mu_0, n, k, D, wavelength)
 
     return get_rmse(r, r_e)
 
 
-def get_D_average_rmse(sid, sample_spectra, k):
-    print("Runnign D average RMSE with params " + str(sid) + " and k " + str(k))
+def get_D_avg_refl_error(sid, sample_spectra, index, k):
+    """
+    Gets average reflectance error over range of grain sizes
+    :param sid: Spectrum ID
+    :param sample_spectra: Pandas DataFrame of data
+    :param index: Index of wavelength to check error on
+    :param k: k value to evaluate
+    """
 
     d_min, d_max = get_grain_sizes(sid, sample_spectra)
-    # D = d_min
     grain_sizes = list(range(int(d_min), int(d_max)))
+
+    # Get wavelength at wavelength index
+    wavelength = get_wavelengths(sid, sample_spectra)[index]
+    # Get actual reflectance spectra from data, for this wavelength index
+    r = get_reflectance_spectra(sid, sample_spectra)[index]
+
+    n = sids_n[sid]
+
     rmses = []
     for D in grain_sizes:
-        rmse = get_reflectance_rmse(sid, sample_spectra, k, D)
+        rmse = get_reflectance_error(wavelength, r, n, k, D)
         rmses.append(rmse)
     return sum(rmses) / len(rmses)
 
 
 def get_best_k(sid, sample_spectra):
-    # 100,000 values from 10^-14 to 1, in log space
-    k_space = np.logspace(-14, -1, 1e5)
-    pool = multiprocessing.Pool()
-    all_rmses = []
-    func = partial(get_D_average_rmse, sid, sample_spectra)
-    all_rmses = pool.map(func, k_space)
+    min_ks = []
+    min_k_errors = []
 
-    pool.close()
-    pool.join()
-    min_index = all_rmses.index(min(all_rmses))
-    min_rmse = min(all_rmses)
-    min_k = k_space[min_index]
+    c = c_wavelengths[5:9] + c_wavelengths[80:93]
 
-    return min_k, min_rmse
+    for index, l in enumerate(c):
+        print("Getting best k error for SID: " + str(sid) + ", on index " + str(index))
+
+        # 100,000 values from 10^-14 to 1, in log space
+        k_space = np.logspace(-14, -1, 1e5)
+
+        pool = multiprocessing.Pool()
+        l_errors = []
+        func = partial(get_D_avg_refl_error, sid, sample_spectra, index)
+        # Multithread over different values in the k space
+        l_errors = pool.map(func, k_space)
+        pool.close()
+        pool.join()
+
+        min_index = l_errors.index(min(l_errors))
+        min_k = k_space[min_index]
+
+        min_ks.append(min_k)
+        min_k_errors.append(min(l_errors))
+
+    return np.array(min_ks), np.array(min_k_errors)
 
 
 ######################################################
 # Helper functions
 ######################################################
+def get_mu_0(sample_row):
+    """
+    Gets mu_0, cosine of incidence angle 
+    :param sample_row: Pandas series from sample spectra DataFrame
+    """
+    source_angle = sample_row["SourceAngle"].values[0]
+    return get_cosine(source_angle)
+
+
+def get_mu(sample_row):
+    """
+    Gets mu, cosine of detect_angle emission/emergence angle
+    :param sample_row: Pandas series from sample spectra DataFrame
+    """
+    detect_angle = sample_row["DetectAngle"].values[0]
+    return get_cosine(detect_angle)
+
+
 def get_rmse(a, b):
     """
     Get RMSE between 2 Numpy arrays
