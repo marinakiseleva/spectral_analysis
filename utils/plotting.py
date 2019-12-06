0a5350fe90de3b382ba7fc7638aa3b0af05d6b26
Marina Kiseleva <marinaki@vt.edu>
2019-12-06 15:39:33 -0700

    Added comments and print SAD when comparing reflectances

--- utils/plotting.py
+++ utils/plotting.py
@@ -11,12 +11,25 @@ def plot_estimated_versus_actual(SID, spectra_db, m_map, D_map):
     """
     Compares passed-in sample spectra to derived spectra using m and D.
     """
+    def get_SAD(a, b):
+        """
+        Get spectral angle distance:
+            d(i,j) =  (i^T * j) / ( ||i|| ||j|| )
+        :param a: Numpy vector
+        :param b: Numpy vector
+        """
+        n = np.dot(a.transpose(), b)
+        d = np.linalg.norm(a) * np.linalg.norm(b)
+        return np.arccos(n / d)
 
     data_reflectance = get_reflectance_spectra(SID, spectra_db)
     ll = get_log_likelihood(data_reflectance, m_map, D_map)
+    print("For " + sids_names[SID])
     print("Log Likelihood: " + str(round(ll, 3)))
 
     estimated_reflectance = get_r_mixed_hapke_estimate(m_map, D_map)
+
+    print("SAD : " + str(get_SAD(data_reflectance, estimated_reflectance)))
 
     fig, axes = plt.subplots(2, 1, constrained_layout=True)
 
@@ -32,6 +45,12 @@ def plot_estimated_versus_actual(SID, spectra_db, m_map, D_map):
 
 
 def plot_compare(actual, pred, title, interp=False):
+    """
+    Plot images side by side
+    :param actual: Numpy 3D array with 2 dimensions as image and 3d dimension as array of values (for actual image)
+    :param pred: Numpy 3D array with 2 dimensions as image and 3d dimension as array of values (for predicted)
+    :param interp: if Numpy third dimension is not between 0 and 1 values, need to interpolate
+    """
     if interp:
         actual = np.interp(actual, (actual.min(), actual.max()), (0, 1))
         pred = np.interp(pred, (pred.min(), pred.max()), (0, 1))
@@ -43,4 +62,5 @@ def plot_compare(actual, pred, title, interp=False):
     axes[1].imshow(pred)
     axes[1].set_title("Estimated")
     fig.suptitle(title, fontsize=14)
+
     return fig
