5078328414c85acad17f4018f8f48f073db1f22e
Marina Kiseleva <marinaki@vt.edu>
2019-12-07 11:31:15 -0700

    Updated running scripts to output to different directories

--- run_inference.py
+++ run_inference.py
@@ -10,42 +10,57 @@ from utils.plotting import plot_compare
 import numpy as np
 import math
 
-image = generate_image(num_mixtures=5,
-                       num_regions=2,
-                       noise_scale=0.001,
-                       size=4)
+if __name__ == "__main__":
+    num_mixtures = 5
+    grid_res = 4
+    noise_scale = 0.001
+    res = 4
+    iterations = 1000
 
+    # Print metadata
+    print("Generating data with: ")
+    print("\t" + str(num_mixtures) + " unique mixtures")
+    print("\t" + str(noise_scale) + " noise (sigma)")
+    print("\t" + str(grid_res) + " grid resolution")
+    print("\t" + str(res) + " pixel resolution")
+    print("\t" + str(iterations) + " iterations")
 
-m_est, D_est = infer_image(iterations=2,
-                           image=image.r_image)
-m_actual = image.m_image
-D_actual = image.D_image
+    print("Conducting MCMC with: ")
+    print("\t" + str(iterations) + " iterations")
 
+    image = generate_image(num_mixtures=num_mixtures,
+                           grid_res=grid_res,
+                           noise_scale=noise_scale,
+                           res=res)
 
-# Save output
-save_dir = "output/data/"
-np.savetxt(save_dir + "m_actual.txt", m_actual.flatten())
-np.savetxt(save_dir + "D_actual.txt", D_actual.flatten())
-np.savetxt(save_dir + "m_estimated.txt", m_est.flatten())
-np.savetxt(save_dir + "D_estimated.txt", D_est.flatten())
+    m_est, D_est = infer_image(iterations=iterations,
+                               image=image.r_image)
+    m_actual = image.m_image
+    D_actual = image.D_image
 
+    # Save output
+    save_dir = "output/data/independent/"
+    np.savetxt(save_dir + "m_actual.txt", m_actual.flatten())
+    np.savetxt(save_dir + "D_actual.txt", D_actual.flatten())
+    np.savetxt(save_dir + "m_estimated.txt", m_est.flatten())
+    np.savetxt(save_dir + "D_estimated.txt", D_est.flatten())
 
-# Print error
-def get_rmse(a, b):
-    return math.sqrt(np.mean((a - b)**2))
-m_rmse = str(round(get_rmse(m_actual, m_est), 2))
-print("RMSE for m: " + m_rmse)
-D_rmse = str(round(get_rmse(D_actual, D_est), 2))
-print("RMSE for D: " + D_rmse)
+    # Print error
+    def get_rmse(a, b):
+        return math.sqrt(np.mean((a - b)**2))
+    m_rmse = str(round(get_rmse(m_actual, m_est), 2))
+    print("RMSE for m: " + m_rmse)
+    D_rmse = str(round(get_rmse(D_actual, D_est), 2))
+    print("RMSE for D: " + D_rmse)
 
-
-# Plot output
-p = plot_compare(actual=m_actual,
-                 pred=m_est,
-                 title="Mineral assemblages m, as RGB (RMSE: " + m_rmse + ")")
-p.savefig("output/figures/m_compare.png")
-p = plot_compare(actual=D_actual,
-                 pred=D_est,
-                 title="Grain size D, as RGB (RMSE: " + D_rmse + ")",
-                 interp=True)
-p.savefig("output/figures/D_compare.png")
+    # Plot output
+    fig_path = "output/figures/independent/"
+    p = plot_compare(actual=m_actual,
+                     pred=m_est,
+                     title="Mineral assemblages m, as RGB (RMSE: " + m_rmse + ")")
+    p.savefig(fig_path + "m_compare.png")
+    p = plot_compare(actual=D_actual,
+                     pred=D_est,
+                     title="Grain size D, as RGB (RMSE: " + D_rmse + ")",
+                     interp=True)
+    p.savefig(fig_path + "D_compare.png")
--- run_segmented_inference.py
+++ run_segmented_inference.py
@@ -1,29 +1,46 @@
 """
-Main script to generate data and run inference on it. 
+Main script to generate data and run inference on it.
 """
 
 from model.segmentation import segment_image, get_superpixels
 from model.inference import infer_segmented_image
 from preprocessing.generate_data import generate_image
 from utils.plotting import plot_compare
+from utils.constants import NUM_ENDMEMBERS
 
 import numpy as np
 import math
 
-NUM_ENDMEMBERS = 3
 
 if __name__ == "__main__":
-    image = generate_image(num_mixtures=5,
-                           num_regions=4,
-                           noise_scale=0.001,
-                           size=4)
+    num_mixtures = 5
+    grid_res = 4
+    noise_scale = 0.001
+    res = 4
+    iterations = 1000
 
-    graphs = segment_image(iterations=1000,
+    # Print metadata
+    print("Generating data with: ")
+    print("\t" + str(num_mixtures) + " unique mixtures")
+    print("\t" + str(noise_scale) + " noise (sigma)")
+    print("\t" + str(grid_res) + " grid resolution")
+    print("\t" + str(res) + " pixel resolution")
+    print("\t" + str(iterations) + " iterations")
+
+    print("Conducting MCMC with: ")
+    print("\t" + str(iterations) + " iterations")
+
+    image = generate_image(num_mixtures=num_mixtures,
+                           grid_res=grid_res,
+                           noise_scale=noise_scale,
+                           res=res)
+
+    graphs = segment_image(iterations=iterations,
                            image=image.r_image
                            )
     superpixels = get_superpixels(graphs)
 
-    m_and_Ds = infer_segmented_image(iterations=1000,
+    m_and_Ds = infer_segmented_image(iterations=iterations,
                                      superpixels=superpixels)
 
     # Reconstruct image
@@ -47,7 +64,7 @@ if __name__ == "__main__":
     D_actual = image.D_image
 
     # Save output
-    save_dir = "output/data/"
+    save_dir = "output/data/segmented/"
     np.savetxt(save_dir + "m_actual.txt", m_actual.flatten())
     np.savetxt(save_dir + "D_actual.txt", D_actual.flatten())
     np.savetxt(save_dir + "m_estimated.txt", m_est.flatten())
@@ -57,17 +74,18 @@ if __name__ == "__main__":
     def get_rmse(a, b):
         return math.sqrt(np.mean((a - b)**2))
     m_rmse = str(round(get_rmse(m_actual, m_est), 2))
-    print("RMSE for m: " + m_rmse)
     D_rmse = str(round(get_rmse(D_actual, D_est), 2))
+    print("RMSE for m: " + m_rmse)
     print("RMSE for D: " + D_rmse)
 
     # Plot output
+    fig_path = "output/figures/segmented/"
     p = plot_compare(actual=m_actual,
                      pred=m_est,
                      title="Mineral assemblages m, as RGB (RMSE: " + m_rmse + ")")
-    p.savefig("output/figures/m_compare.png")
+    p.savefig(fig_path + "m_compare.png")
     p = plot_compare(actual=D_actual,
                      pred=D_est,
                      title="Grain size D, as RGB (RMSE: " + D_rmse + ")",
                      interp=True)
-    p.savefig("output/figures/D_compare.png")
+    p.savefig(fig_path + "D_compare.png")
