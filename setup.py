78022458ad3cf2f9f38c18ebfb05bd27b5316ecc
Marina Kiseleva <marinaki@vt.edu>
2020-06-17 14:37:00 -0700

    Updated setup file

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from setuptools import setup, find_packages
 setup(
-    name="specpgm",
+    name="hsiinfer",
     version="0.1",
     packages=find_packages(),
     author="Marina Kisley",
     author_email="marinaki@email.arizona.edu",
-    description="Probabilistic graphical model for spectral analysis."
+    description="Infering mineral assemblage and grain size for hyperspectral images."
 )
