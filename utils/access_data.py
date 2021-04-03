eeccab7769854baf630db1a55c527eb5e147ed2f
Marina Kiseleva <marinaki@vt.edu>
2021-04-03 11:01:20 -0700

    Updated and cleaning up access of CRISM wavelengths; moved some functionality to preprocessing.

--- utils/access_data.py
+++ utils/access_data.py
@@ -21,69 +21,71 @@ def get_endmember_wavelengths(CRISM_match=True):
     return N_WAVELENGTHS
 
 
-def match_lists(CRISM_wavelengths, USGS_wavelengths):
-    """
-    Keeps wavelengths that are same in 2 lists. Tested on wavelength vectors which contains same subset of values.   
-    """
-    new_CRISM = []
-    new_USGS = []
-    U_index = 0
-    for index, C_val in enumerate(CRISM_wavelengths):
-        if U_index == len(USGS_wavelengths):
-            break
-
-        U_val = USGS_wavelengths[U_index]
-
-        if C_val >= U_val:
-            U_index += 1
-            new_CRISM.append(C_val)
-            new_USGS.append(U_val)
-
-    return new_CRISM, new_USGS
-
-
-def record_reduced_spectra(CRISM_w_file):
-    """
-    Saves the wavelengths that are found to be as equal as possible between USGS (olivine Fo80)  and CRISM (random pixel from passed-in image) spectra.  The unique wavelengths are saved per data source because this is how their spectral values will be accessed.
-    :param CRISM_w_file: Path and .txt file name of CRISM z-profile for a single pixel, saved from CAT ENVI
-    """
-    # Get data
-    ss = get_data()
-    USGS_data = get_USGS_data("olivine (Fo80)", CRISM_match=False)
-    USGS_wavelengths = USGS_data['wavelength'].values.tolist()
-    CRISM_wavelengths = get_CRISM_wavelengths(CRISM_w_file)
-
-    # Match USGS to CRISM
-    CRISM_reduced, USGS_reduced = match_lists(CRISM_wavelengths, USGS_wavelengths)
-
-    path = MODULE_DIR + "/utils/FILE_CONSTANTS/"
-    with open(path + 'RW_USGS.pickle', 'wb') as f:
-        pickle.dump(USGS_reduced, f)
-    with open(path + 'RW_CRISM.pickle', 'wb') as f:
-        pickle.dump(CRISM_reduced, f)
-
-    rmse = np.sqrt(np.mean((np.array(CRISM_reduced) - np.array(USGS_reduced))**2))
-    print("CRISM reduced from " + str(len(CRISM_wavelengths)) +
-          " to " + str(len(CRISM_reduced)))
-    print("USGS reduced from " + str(len(USGS_wavelengths)) +
-          " to " + str(len(USGS_reduced)))
-    print("RMSE between normalized wavelength vectors: " + str(rmse))
-
-
 """
 CRISM data access
 """
 
 
-def get_CRISM_img_wavelengths(wavelengths_file):
+def get_CRISM_wavelengths():
     """
-    Get wavelengths that we are using in the CRISM image 
+    Get full set of wavelengths of CRISM data.
     """
-    with open(wavelengths_file, 'rb') as handle:
-        img_wavelengths = pickle.load(handle)
 
+    filename = DATA_DIR + "PREPROCESSED_DATA/" + 'CRISM_wavelengths.pickle'
+    with open(filename, 'rb') as handle:
+        wavelengths = pickle.load(handle)
+    return wavelengths
+
+
+def get_CRISM_RWs():
+    """
+    Get reduced wavelengths of CRISM; those matched to lab spectra
+    """
     crism_w = MODULE_DIR + "/utils/FILE_CONSTANTS/RW_CRISM.pickle"
     with open(crism_w, 'rb') as handle:
+        RW_CRISM = pickle.load(handle)
+    return RW_CRISM
+
+
+# def get_CRISM_img_wavelengths(wavelengths_file):
+#     """
+#     Get wavelengths that we are using in the CRISM image
+#     """
+#     with open(wavelengths_file, 'rb') as handle:
+#         img_wavelengths = pickle.load(handle)
+
+#     crism_w = MODULE_DIR + "/utils/FILE_CONSTANTS/RW_CRISM.pickle"
+#     with open(crism_w, 'rb') as handle:
+#         # CRISM reduced wavelengths to keep.
+#         RW_CRISM = pickle.load(handle)
+
+#     keep_indices = []  # indices of spectra to keep.
+#     for index, w in enumerate(img_wavelengths):
+#         if w in RW_CRISM:
+#             keep_indices.append(index)
+
+#     if len(keep_indices) != len(RW_CRISM):
+#         raise ValueError("Issue normalizing wavelengths of CRISM img. ")
+
+#     return [img_wavelengths[i] for i in keep_indices]
+
+
+def get_CRISM_data(image_file):
+    """
+    Gets CRISM data this directory & name.
+    :param image_file: File name of Pickled image.
+    """
+
+    if 'pickle' not in image_file:
+        return ValueError("get_CRISM_data only handles pickles.")
+    path = DATA_DIR + "PREPROCESSED_DATA/"
+    with open(path + image_file, 'rb') as handle:
+        loaded_img = pickle.load(handle)
+
+    with open(path + "CRISM_wavelengths.pickle", 'rb') as handle:
+        img_wavelengths = pickle.load(handle)
+
+    with open(path + "FILE_CONSTANTS/RW_CRISM.pickle", 'rb') as handle:
         # CRISM reduced wavelengths to keep.
         RW_CRISM = pickle.load(handle)
 
@@ -95,70 +97,14 @@ def get_CRISM_img_wavelengths(wavelengths_file):
     if len(keep_indices) != len(RW_CRISM):
         raise ValueError("Issue normalizing wavelengths of CRISM img. ")
 
-    return [img_wavelengths[i] for i in keep_indices]
+    # Create new image with only these wavelengths
+    newimg = np.zeros((loaded_img.shape[0], loaded_img.shape[1], len(RW_CRISM)))
+    for xindex, row in enumerate(loaded_img):
+        for yindex, val in enumerate(row):
+            # Only keep values with reduced wavelengths
+            newimg[xindex, yindex] = np.take(val, keep_indices)
 
-
-def get_CRISM_data(image_file, wavelengths_file, CRISM_match=True):
-    """
-    Gets CRISM data this directory & name.
-    If matching to CRISM - make sure that record_reduced_spectra was called before this so that we know what wavelengths to match to.
-    :param image_file: Full dir and file name of Pickled image.
-    :param wavelengths_file: Full dir and file name of wavelength values for image
-    :param CRISM_match: filter spectra to same range as endmembers
-    """
-    if 'pickle' not in image_file:
-        return ValueError("get_CRISM_data only handles pickles.")
-
-    with open(image_file, 'rb') as handle:
-        loaded_img = pickle.load(handle)
-
-    if CRISM_match:
-        """
-        Keep all wavelengths in image that are in RW_CRISM
-        """
-        with open(wavelengths_file, 'rb') as handle:
-            img_wavelengths = pickle.load(handle)
-
-        crism_w = MODULE_DIR + "/utils/FILE_CONSTANTS/RW_CRISM.pickle"
-        with open(crism_w, 'rb') as handle:
-            # CRISM reduced wavelengths to keep.
-            RW_CRISM = pickle.load(handle)
-
-        keep_indices = []  # indices of spectra to keep.
-        for index, w in enumerate(img_wavelengths):
-            if w in RW_CRISM:
-                keep_indices.append(index)
-
-        if len(keep_indices) != len(RW_CRISM):
-            raise ValueError("Issue normalizing wavelengths of CRISM img. ")
-
-        # Create new image with only these wavelengths
-        newimg = np.zeros((loaded_img.shape[0], loaded_img.shape[1], len(RW_CRISM)))
-        for xindex, row in enumerate(loaded_img):
-            for yindex, val in enumerate(row):
-                # Only keep values with reduced wavelengths
-                newimg[xindex, yindex] = np.take(val, keep_indices)
-
-        return newimg
-
-    return loaded_img
-
-
-def get_CRISM_wavelengths(file):
-    """
-    Get the wavelengths of CRISM data from saved file. Handles pickle and CSV.
-    :param file: Full file name of CRISM wavelengths (including path)
-    """
-    if file is None:
-        return ValueError("Must pass in file.")
-
-    if ".csv" in file:
-        data = pd.read_csv(file, header=None)
-        wavelengths = data[0]
-    elif '.pickle' in file:
-        with open(file, 'rb') as handle:
-            wavelengths = pickle.load(handle)
-    return wavelengths
+    return newimg
 
 
 """
@@ -191,7 +137,7 @@ def get_USGS_endmember_k(endmember):
 def get_USGS_data(endmember, CRISM_match=False):
     """
     Get USGS spectral reflectance data for this endmember, as Pandas DataFrame
-    If CRISM_match = True, then keep only reflectance values for wavelengths that we determined to keep; those in RW_USGS.  
+    If CRISM_match = True, then keep only reflectance values for wavelengths in RW_USGS.  
     """
     # Normalize filename
     endmember = endmember.lower()
@@ -207,8 +153,9 @@ def get_USGS_data(endmember, CRISM_match=False):
     data.loc[data['reflectance'] < 0, 'reflectance'] = 0
 
     if CRISM_match:
+        path = DATA_DIR + "PREPROCESSED_DATA/FILE_CONSTANTS/"
         # Only keep rows with reduced wavelengths
-        with open(MODULE_DIR + "/utils/FILE_CONSTANTS/RW_USGS.pickle", 'rb') as handle:
+        with open(path + "RW_USGS.pickle", 'rb') as handle:
             RW_USGS = pickle.load(handle)
         data = data[data['wavelength'].isin(RW_USGS)]
 
